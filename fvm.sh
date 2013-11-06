5d07c002a91c88e8bbee63ae6daac8683994842e
foonyah <account@liberty-technology.biz>
2013-11-07 08:30:03 +0900

    for test

--- fvm.sh
+++ fvm.sh
@@ -3,41 +3,6 @@
 # To use source this file from your bash profile
 
 # Implemented by Yoshitaka Sakamoto <sakamoto@liberty-technology.biz>
-
-# Set workspace directory ( Default: cwd )
-if [ -z "$WORKSPACE" ];then
-  WORKSPACE=`echo $0 | sed "s/\/[^\/]*$//g"`
-fi
-if [ "sh" == "$WORKSPACE" ];then
-  INSTALL=1
-  WORKSPACE=`pwd`
-fi
-
-# Set nvm directory ( Default: $WORKSPACE/.nvm )
-if [ -z "$NVM_PATH" ];then
-  NVM_PATH=.nvm
-fi
-if [ -z "$NODE_VER" ];then
-  NODE_VER=0.10.21
-fi
-NVM_HOME=$WORKSPACE/$NVM_PATH
-
-if [ -n "$INSTALL" ];then
-  git clone https://github.com/creationix/nvm.git $NVM_HOME
-  source $NVM_HOME/nvm.sh
-  nvm install $NODE_VER
-fi
-
-if [ !-d "$NVM_HOME/node_modules" ];then
-  mkdir node_modules
-fi
-
-if [ -n "$INSTALL" ];then
-  npm install named-argv@0.1.0
-  npm install micro-pipe@0.1.0
-  fvm install
-fi
-
 fvm () {
   node_source
   node fvm.js $@
@@ -61,3 +26,40 @@ foonyah () {
   node_source
   sudo node $start
 }
+
+# Set workspace directory ( Default: cwd )
+if [ -n "$WORKSPACE" ];then
+  mkdir -p $WORKSPACE
+  cd $WORKSPACE
+fi
+if [ "sh" == "$WORKSPACE" ];then
+  INSTALL=1
+  WORKSPACE=`pwd`
+  # execute this position
+fi
+
+
+# Set nvm directory ( Default: .nvm )
+if [ -z "$NVM_PATH" ];then
+  NVM_PATH=.nvm
+fi
+if [ -z "$NODE_VER" ];then
+  NODE_VER=0.10.21
+fi
+NVM_HOME=$WORKSPACE/$NVM_PATH
+
+if [ -n "$INSTALL" ];then
+  git clone https://github.com/creationix/nvm.git $NVM_PATH
+  source $NVM_PATH/nvm.sh
+  nvm install v$NODE_VER
+fi
+
+if [ ! -d "$NVM_PATH/node_modules" ];then
+  mkdir node_modules
+fi
+
+if [ -n "$INSTALL" ];then
+  npm install named-argv@0.1.0
+  npm install micro-pipe@0.1.0
+  fvm install
+fi
