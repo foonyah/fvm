e2e788dbcc847d0351aed4d2d9d0bd22df6e8cb6
foonyah <account@liberty-technology.biz>
2013-11-06 22:29:41 +0900

    for test

---
+++ fvm.js
@@ -0,0 +1,68 @@
+/***/
+var assert = require('assert'), path = require('path'), fs = require('fs');
+var argv = require('named-argv'), micropipe = require('micro-pipe');
+var platform = require('os').platform;
+
+var Mongo = {
+  Dist: 'http://fastdl.mongodb.org/',
+  Cver: '2.4.8'
+};
+
+var pwd = process.cwd(), tasks = [];
+var mem = null;
+tasks.push(function(next) {
+  // open archive file
+  mem = path.join(pwd, 'mongodb' + ext());
+  fs.open(mem, next)
+
+}, function(err, fd, next) {
+  // download mongodb
+  require('http').get(mongoArchive(), function(err, res) {
+    res.on('data', function(d) {
+      fd.write(d);
+    }).on('end', function() {
+      fd.close(next);
+    });
+  });
+
+}, function(err, next) {
+  // decompress
+  require('child_process').exec(decompress(mem)).on('exit', next)
+
+}, function() {
+  // mkdir "foonyah", "foonyah-plugins"
+  ['node_modules/foonyah', 'node_modules/foonyah-plugins'].forEach(function(f) {
+    var d = path.resolve('./' + f);
+    try {
+      if(!fs.statSync(d).isDirectory())
+        throw 'mkdir';
+    } catch(e) {
+      fs.mkdirSync(d);
+    }
+  });
+
+});
+
+micropipe(tasks.concat(function() {
+  process.exit(0);
+}))
+
+function mongoArchive() {
+  return Mongo.Dist + (function() {
+    if(platform === 'win32')
+      return 'win32/mongodb-win32-x86_64-2008plus-' + Mongo.Cver + '.zip';
+    if(platform === 'darwin')
+      return 'osx/mongodb-osx-x86_64-' + Mongo.Cver + '.tgz';
+    if(platform === 'sunos')
+      return 'sunos5/mongodb-sunos5-x86_64-' + Mongo.Cver + '.tgz';
+    return 'linux/mongodb-linux-x86_64-' + Mongo.Cver + '.tgz';
+  })();
+}
+
+function ext() {
+  return platform === 'win32' ? '.zip': '.tgz';
+}
+
+function decompress(f) {
+  return (ext() == '.tgz' ? 'tar -zxvf': 'unzip') + ' ' + f;
+}
--- fvm.sh
+++ fvm.sh
@@ -4,13 +4,13 @@
 
 # Implemented by Yoshitaka Sakamoto <sakamoto@liberty-technology.biz>
 
-echo "$0"
-echo `pwd`
-exit 0
-
 # Set workspace directory ( Default: cwd )
 if [ -z "$WORKSPACE" ];then
   WORKSPACE=`echo $0 | sed "s/\/[^\/]*$//g"`
+fi
+if [ "sh" == "$WORKSPACE" ];then
+  INSTALL=1
+  WORKSPACE=`pwd`
 fi
 
 # Set nvm directory ( Default: $WORKSPACE/.nvm )
@@ -22,9 +22,21 @@ if [ -z "$NODE_VER" ];then
 fi
 NVM_HOME=$WORKSPACE/$NVM_PATH
 
-git clone https://github.com/creationix/nvm.git $NVM_HOME
-source $NVM_HOME/nvm.sh
-nvm install $NODE_VER
+if [ -n "$INSTALL" ];then
+  git clone https://github.com/creationix/nvm.git $NVM_HOME
+  source $NVM_HOME/nvm.sh
+  nvm install $NODE_VER
+fi
+
+if [ !-d "$NVM_HOME/node_modules" ];then
+  mkdir node_modules
+fi
+
+if [ -n "$INSTALL" ];then
+  npm install named-argv@0.1.0
+  npm install micro-pipe@0.1.0
+  fvm install
+fi
 
 fvm () {
   node_source
