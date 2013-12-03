d914018d4c2c6e1ce235f69f472d56a7c43fdd81
foonyah <account@liberty-technology.biz>
2013-12-03 17:49:08 +0900

    test

--- fvm.sh
+++ fvm.sh
@@ -9,6 +9,7 @@
 # Special Thanks
 #  [East Cloud Inc](http://www.east-cloud.co.jp/)
 
+# tunneling execution
 if [ "sh" = "$0" ];then
   curl https://raw.github.com/foonyah/fvm/master/fvm.sh > .fvm.sh
   chmod 755 .fvm.sh
@@ -16,26 +17,22 @@ if [ "sh" = "$0" ];then
   exit 0
 fi
 
+# METHODS "fvm" "fpm" "foonyah" and for internally, "node_source"
 fvm () {
   node_source
-  if [ "--all" = "$2" ];then
+  if [ "$2" = "--package" ];then
     node $FVM_DIRC/fvm.js $@ --config=$FVM_DIRC/package.json
-  else
-    node $FVM_DIRC/fvm.js $@
+    exit 0
   fi
+  if [ "$2" = "1" ];then
+    exit 0
+  fi
+  node $FVM_DIRC/fvm.js $@
 }
-
 fpm () {
   node_source
   node $FVM_DIRC/fpm.js $@
 }
-
-node_source () {
-  echo "referring nvm... ($NVM_DIRC/nvm.sh)"
-  source $NVM_DIRC/nvm.sh
-  nvm use v$NODE_VER
-}
-
 foonyah () {
   start=$1
   if [ -z "$start" ];then
@@ -44,7 +41,11 @@ foonyah () {
   node_source
   sudo node $start
 }
-
+node_source () {
+  echo "referring nvm... ($NVM_DIRC/nvm.sh)"
+  source $NVM_DIRC/nvm.sh
+  nvm use v$NODE_VER
+}
 # Set workspace directory ( Default: cwd )
 if [ -n "$WORKSPACE" ];then
   mkdir -p $WORKSPACE
@@ -52,26 +53,25 @@ if [ -n "$WORKSPACE" ];then
 else
   WORKSPACE=`pwd`
 fi
-
 # execute this position
 if [ "install" = "$1" ];then
-  INSTALL=1
+  INSTALL=$2
+  if [ -z "$INSTALL" ];then
+    INSTALL=--package
+  fi
 fi
-
 # Set nvm directory ( Default: .nvm )
 if [ -z "$NVM_DIRC" ];then
   NVM_DIRC=.nvm
 fi
 if [ -z "$NODE_VER" ];then
-  NODE_VER=0.10.21
+  NODE_VER=0.10.22
 fi
 NVM_HOME=$WORKSPACE/$NVM_DIRC
-
 # Set nvm directory ( Default: .fvm )
 if [ -z "$FVM_DIRC" ];then
   FVM_DIRC=.fvm
 fi
-
 if [ -n "$INSTALL" ];then
   git clone https://github.com/creationix/nvm.git $NVM_DIRC
   git clone https://github.com/foonyah/fvm.git $FVM_DIRC
@@ -79,12 +79,10 @@ if [ -n "$INSTALL" ];then
   echo "Installing node v$NODE_VER..."
   nvm install v$NODE_VER
 fi
-
 if [ ! -d "./node_modules" ];then
   echo "Create node_module directory."
   mkdir -p node_modules
 fi
-
 if [ -n "$INSTALL" ];then
   # require npm modules
   echo "Installing require npm packages..."
@@ -93,5 +91,5 @@ if [ -n "$INSTALL" ];then
     npm install grunt-runner@0.9.1
     npm install grunt-tree-prepare@0.9.1
   fi
-  fvm install --all
+  fvm install $INSTALL
 fi
